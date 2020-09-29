92f3353021eed8a4200a759814f717a53f7b1ba9
Evgenii <63541855+evgenyakut@users.noreply.github.com>
2020-09-29 13:50:29 +0200

    Update projeckt_git0.py

--- module_0/projeckt_git0.py
+++ module_0/projeckt_git0.py
@@ -5,11 +5,13 @@ def score_game(game_core):
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1,101, size=(1000))
+    
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
+
 
 def game_core(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
